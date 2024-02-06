d9c8c2da7c3430228cd9fbb7c79fbd778bed8a06
Khaled Hussein <husseink1@southernct.edu>
2024-02-05 23:27:38 -0500

    calling endpoints and class to functional conversion

---
+++ frontend/src/components/Room.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+
+export const Room = () => {
+    const [VotesToSkip, setVotesToSkip] = useState(2);
+    const [guestCanPause, setGuestCanPause] = useState(false);
+    const [isHost, setIsHost] = useState(false);
+
+    const { roomCode } = useParams();
+
+    useEffect(() => {
+        getRoomDetails();
+    }, [roomCode]);
+
+    const getRoomDetails = () => {
+        fetch('/api/get-room' + '?code=' + roomCode)
+        .then((response) => response.json())
+        .then((data) => {
+            setVotesToSkip(data.votes_to_skip);
+            setGuestCanPause(data.guest_can_pause);
+            setIsHost(data.is_host);
+        })
+        .catch((error) => {
+            console.error('Error fetching room details:', error);
+        });
+    };
+    
+    return (
+    <div>
+        <h3>{roomCode}</h3>
+        <p>Votes: {VotesToSkip}</p>
+        <p>Guest Can Pause: {guestCanPause.toString()}</p>
+        <p>Host: {isHost.toString()}</p>
+    </div>
+    );
+};
+export default Room;
